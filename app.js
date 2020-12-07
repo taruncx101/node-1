6ca453390d9254ba0ad3c0b2f0997587447f24af
Tarun Gorai <goraitarunkumar@gmail.com>
2020-12-07 12:03:16 +0530

    adding a post request

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/user');
 
 const app = express();
+
+app.use(bodyParser.json()); // for parsing applcation/json data
 
 app.use('/user', userRoutes)
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 exports.getUsers = (req, res, next) => {
-    res.json({
+    res.status(200)
+        .json({
         users: [
             {
                 name: 'tarun',
@@ -8,4 +9,18 @@ exports.getUsers = (req, res, next) => {
         ],
         total: 1
     })
+}
+
+exports.createUser = (req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    res.status(201)
+        .json({
+            message: "User created successfully",
+            user: {
+                id: 1,
+                name,
+                email,
+            },
+    });
 }
