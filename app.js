b2bea327ce808a3bbb8b0b1b6c80bb528823a1ce
Tarun Gorai <goraitarunkumar@gmail.com>
2020-12-07 12:16:12 +0530

    set up cors policies

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const app = express();
 
 app.use(bodyParser.json()); // for parsing applcation/json data
 
+//set up cors policies
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+})
+
 app.use('/user', userRoutes)
 
 app.listen(8080);
