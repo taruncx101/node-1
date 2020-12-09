7cbdcc563b94ec69dd5f4a1ff309366dd0c71db0
Tarun Gorai <goraitarunkumar@gmail.com>
2020-12-09 16:08:57 +0530

    changed after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-source ~/.profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/app/node-server && aws s3 cp s3://codepipeline-tarun/node-env/.env .
 cd /home/app/node-server && npm install
 pm2 list
