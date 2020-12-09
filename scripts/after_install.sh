118d32c71590ad1a91907e0fbafb41aa16bef8cc
Tarun Gorai <goraitarunkumar@gmail.com>
2020-12-09 15:57:27 +0530

    Fixed the after install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd home/app/node-server && aws s3 cp s3://codepipeline-tarun/node-env/.env .
-cd home/app/node-server && npm install
+cd /home/app/node-server && aws s3 cp s3://codepipeline-tarun/node-env/.env .
+cd /home/app/node-server && npm install
